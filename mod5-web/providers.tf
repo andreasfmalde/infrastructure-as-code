0736c5b958d4bf2a03857d3faaaea5b948f288cb
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-10-03 01:39:28 +0200

    mod5-web: Added backend configuration

---
+++ mod5-web/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "afmbackend-rg"
+    storage_account_name = "afmbackendsam7q5ab"
+    container_name       = "tfstate"
+    key                  = "mod5-web.tfstate"
+  }
+    
+}
